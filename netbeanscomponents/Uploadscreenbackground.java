ac9e1be5fb0aea7e3dbff63b8fa99f833b5a7a3b
umutozdemr <88161164+umutozdemr@users.noreply.github.com>
2021-07-30 10:31:36 +0300

    Add files via upload

---
+++ netbeanscomponents/Uploadscreenbackground.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netbeanscomponents;
+
+import java.awt.Graphics;
+import java.beans.*;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author DELL
+ */
+public class Uploadscreenbackground extends JPanel implements Serializable {
+    
+    public static final String PROP_SAMPLE_PROPERTY = "sampleProperty";
+    
+    private String sampleProperty;
+    ImageIcon img;
+    private PropertyChangeSupport propertySupport;
+    
+    public Uploadscreenbackground() {
+        propertySupport = new PropertyChangeSupport(this);
+        img = new ImageIcon(getClass().getResource("uploadscreenbackground.png"));
+    }
+    
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
+    
+    public void setSampleProperty(String value) {
+        String oldValue = sampleProperty;
+        sampleProperty = value;
+        propertySupport.firePropertyChange(PROP_SAMPLE_PROPERTY, oldValue, sampleProperty);
+    }
+    
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
+    
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
+    }
+    
+   
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); 
+        g.drawImage(img.getImage(), 0, 0, null);
+          
+    }
+    
+}
